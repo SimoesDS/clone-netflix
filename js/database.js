cf39ad4c06ed2574803993bbbcf7d2bbdd58fee3
SimoesDS <simoes.sdgo@gmail.com>
2021-02-12 23:45:37 -0300

    puts the movie in focus when clicked

--- js/database.js
+++ js/database.js
@@ -1,41 +1,41 @@
 var database = [
     {
-        "name": "A Colméia",
+        "title": "A Colméia",
         "synopsis": "O filme conta a história de Adão, um conselheiro do acampamento adolescente que acorda dentro de uma cabine fechada com tábuas, sem absolutamente nenhuma memória de quem ele é ou onde ele está. As únicas pistas sobre quem ele é e o que está acontecendo são mensagens rabiscadas que Adam fez para si mesmo sobre as paredes e as mudanças perturbadoras com sua fisiologia. Quando ele começa a juntar os eventos que o levaram à sua estranha prisão, ele percebe que as únicas lembranças que ele tem não são suas, e elas podem ser a chave para sua sobrevivência.",
         "imdb": 5.2,
         "genre": "Terror, Ficção científica, Thriller",
         "image": "./img/capa-a-colmeia.jpg"
     },
     {
-        "name": "Coringa",
+        "title": "Coringa",
         "synopsis": "Arthur Fleck trabalha como palhaço para uma agência de talentos e, toda semana, precisa comparecer a uma agente social, devido aos seus conhecidos problemas mentais. Após ser demitido, Fleck reage mal à gozação de três homens em pleno metrô e os mata. Os assassinatos iniciam um movimento popular contra a elite de Gotham City, da qual Thomas Wayne é seu maior representante.",
         "imdb": 8.5,
         "genre": "Crime, Thriller, Drama",
         "image": "./img/capa-coringa.jpg"
     },
     {
-        "name": "Sniper Americano",
+        "title": "Sniper Americano",
         "synopsis": "História real de Chris Kyle, atirador de elite das forças especiais da marinha dos Estados Unidos. Durante a guerra do Iraque, sua missão era uma só- proteger seus companheiros. Seu dever fez dele um dos maiores atiradores da história americana. Sua precisão salva inúmeras vidas, mas também o torna um alvo preferencial. Quando Kyle finalmente volta para casa, ele descobre que não tem como deixar a guerra para trás.",
         "imdb": 6.8,
         "genre": "Guerra, Ação",
         "image": "./img/capa-sniper-americano.jpg"
     },
     {
-        "name": "Um homem comum",
+        "title": "Um homem comum",
         "synopsis": "Criminoso de guerra e procurado mundo afora, um general que vive escondido há anos é transferido para outro lugar. Lá, ele conhece Tanja, sua nova empregada, e um estranho relacionamento surge entre eles. Quando a calmaria parece estar se estabelecendo na vida do general, a camareira lê a notícia de que há uma recompensa milionária para quem souber onde o fugitivo está. Preocupado com a segurança da garota, o atormentado homem acredita que a solução será enfrentar o passado e voltar para casa.",
         "imdb": 5.5,
         "genre": "Ação, Drama, Guerra",
         "image": "./img/capa-um-homem-comum.jpg"
     },
     {
-        "name": "Zona de combate",
+        "title": "Zona de combate",
         "synopsis": "Em um futuro próximo, um piloto de drone e um androide ultrassecreto trabalham lado a lado no campo de batalha para evitar um ataque nuclear.",
         "imdb": 5.4,
         "genre": "Thriller, Ação, Ficção científica",
         "image": "./img/capa-zona-de-combate.jpg"
     },
     {
-        "name": "Tokyo Ghoul",
+        "title": "Tokyo Ghoul",
         "synopsis": "Estranhos assassinatos começam a acontecer em Tokyo. Devido a evidência líquida nos casos, a polícia concluiu que os ataques são resultados de uma criatura que se alimenta de outros seres, um \"ghoul\" do tipo \"eater\". Dois amigos de faculdade, Kaneki e Hide, criam a teoria de que os ghouls estão imitando os humanos, por isso nunca são vistos ou capturados. Eles nem imaginam que essa teoria pode ser verdade e a coisa sairá do controle, pois Kaneki acaba se envolvendo, sem saber, com uma dessas criaturas, que ao atacar o garoto acaba morrendo acidentalmente, mas o deixa seriamente ferido e a ponto de morrer. A única salvação? Um transplante envolvendo a própria criatura, mas que poderá trazer consequências terríveis para Kaneki. O que acontecerá a partir de agora?",
         "imdb": 7.9,
         "genre": "Action & Adventure, Animação, Drama, Mistério",
--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 const carrouselElement = document.getElementById('owl-carousel');
+const mainMovieElement = document.getElementsByClassName('filme-principal')[0];
+const titleElement = document.getElementsByClassName('titulo')[0];
+const synopisElement = document.getElementsByClassName('sinopse')[0];
 
-for (const data of database) {
+const handlerMainMovie = ({ title, synopsis, imdb, genre, image}) => {
+    mainMovieElement.style.backgroundImage = `url('${image}')`;
+    titleElement.textContent = title;
+    synopisElement.textContent = synopsis;
+};
+
+database.forEach(({ title, synopsis, imdb, genre, image}) => {
     const div = document.createElement('div')
     div.setAttribute('class', 'item');
     
     const img = document.createElement('img')
-    img.setAttribute('src', data.image);
+    img.setAttribute('src', image);
+    img.addEventListener('click', () => {
+        handlerMainMovie({ title, synopsis, imdb, genre, image});
+    })
     
     div.appendChild(img);
     carrouselElement.appendChild(div);
-}
+});
